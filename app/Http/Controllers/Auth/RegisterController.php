386d30c748d81f849be0ea995745aff77b1ada36
Shift <shift@laravelshift.com>
2022-02-28 16:03:06 +0000

    Namespace models under App\Models

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use function bcrypt;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
